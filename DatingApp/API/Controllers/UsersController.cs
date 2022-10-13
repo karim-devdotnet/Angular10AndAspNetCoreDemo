8bff7faf8c7ca1cee2d6b72d66ff03cac2b89e15
Abdelkarim Abdallah <abdelkarim.esti@gmail.com>
2022-10-13 13:51:38 +0200

    First commit

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUsers(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
