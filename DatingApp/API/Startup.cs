ca00d3611f9be242d8af375c94e84add333d71e6
Abdelkarim Abdallah <abdelkarim.esti@gmail.com>
2022-11-04 08:37:04 +0100

    Client added

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -37,6 +37,9 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
